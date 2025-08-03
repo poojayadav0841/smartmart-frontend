e4c555bb768f511c24b7abdc77dd01683ef36065
pooja yadav <252poojayadav@gmail.com>
2025-08-03 22:03:37 +0530

    solving routing problem

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
 import { provideRouter, withHashLocation } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes, withHashLocation()), // ✅ enables hash-based routing
-    provideHttpClient(),
-  ],
-}).catch((err) => console.error(err));
+  providers: [provideRouter(routes, withHashLocation())],
+});
