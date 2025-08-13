7b6e9622053129b3cada3aa4907a090e621e8d01
pooja yadav <252poojayadav@gmail.com>
2025-08-14 00:09:09 +0530

    fix url problem

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,57 +1,59 @@
-import { HttpClient , HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserAuthService } from './user-auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  PATH_OF_API = 'http://localhost:8080';
 
-  PATH_OF_API = "http://localhost:9090";
+  requestHeader = new HttpHeaders({ 'No-Auth': 'True' });
 
-  requestHeader = new HttpHeaders(
-    { "No-Auth": "True" }
-  );
+  constructor(
+    private httpclient: HttpClient,
+    private userAuthService: UserAuthService
+  ) {}
 
-  constructor(private httpclient : HttpClient,
-    private userAuthService : UserAuthService
-  ) { }
-
-
-  public register(registerData : any){
-    return this.httpclient.post(this.PATH_OF_API + '/registerNewUser', registerData);
+  public register(registerData: any) {
+    return this.httpclient.post(
+      this.PATH_OF_API + '/registerNewUser',
+      registerData
+    );
   }
 
-  public login(loginData : any){
-    return this.httpclient.post(this.PATH_OF_API + "/authenticate",loginData , {headers : this.requestHeader});
+  public login(loginData: any) {
+    return this.httpclient.post(this.PATH_OF_API + '/authenticate', loginData, {
+      headers: this.requestHeader,
+    });
   }
 
   public forUser() {
-  const token = this.userAuthService.getToken();
-  const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    const token = this.userAuthService.getToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-  return this.httpclient.get(this.PATH_OF_API + '/forUser', {
-    headers: headers,
-    responseType: 'text',
-  });
-}
+    return this.httpclient.get(this.PATH_OF_API + '/forUser', {
+      headers: headers,
+      responseType: 'text',
+    });
+  }
   public forAdmin() {
-  const token = this.userAuthService.getToken();
-  const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    const token = this.userAuthService.getToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-  return this.httpclient.get(this.PATH_OF_API + '/forAdmin', {
-    headers: headers,
-    responseType: 'text',
-  });
-}
-  public roleMatch(allowedRoles : any[]) : boolean {
+    return this.httpclient.get(this.PATH_OF_API + '/forAdmin', {
+      headers: headers,
+      responseType: 'text',
+    });
+  }
+  public roleMatch(allowedRoles: any[]): boolean {
     let isMatch = false;
-    const userRoles:any = this.userAuthService.getRoles();
+    const userRoles: any = this.userAuthService.getRoles();
 
-    if(userRoles != null && userRoles){
-      for(let i=0; i<userRoles.length; i++){
-        for(let j=0; j<allowedRoles.length; j++){
-          if(userRoles[i].roleName === allowedRoles[j]){
+    if (userRoles != null && userRoles) {
+      for (let i = 0; i < userRoles.length; i++) {
+        for (let j = 0; j < allowedRoles.length; j++) {
+          if (userRoles[i].roleName === allowedRoles[j]) {
             isMatch = true;
             return isMatch;
           }
